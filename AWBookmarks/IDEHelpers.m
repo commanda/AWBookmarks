a2aa76d23f9950334584eaf2d4e0878c7527d1ee
Amanda Wixted <amanda@amandawixted.com>
2016-05-20 21:49:42 -0500

    added IDEHelpers with a bunch of interfaces in the header - not sure how i'll use them yet

---
+++ AWBookmarks/IDEHelpers.m
@@ -0,0 +1,13 @@
+//
+//  IDEHelpers.m
+//  AWBookmarks
+//
+//  Created by Amanda Wixted on 5/20/16.
+//  Copyright © 2016 Amanda Wixted. All rights reserved.
+//
+
+#import "IDEHelpers.h"
+
+@implementation IDEHelpers
+
+@end
