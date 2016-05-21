c94d0625de2be6f23592e0daae89aafd2ed14f7a
Amanda Wixted <amanda@amandawixted.com>
2016-05-20 22:08:29 -0500

    added model class AWBookmarkEntry

---
+++ AWBookmarks/AWBookmarkEntry.h
@@ -0,0 +1,17 @@
+//
+//  AWBookmarkEntry.h
+//  AWBookmarks
+//
+//  Created by Amanda Wixted on 5/20/16.
+//  Copyright © 2016 Amanda Wixted. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AWBookmarkEntry : NSObject
+
+@property (nonatomic, copy) NSString* filePath;
+@property (nonatomic, assign) NSUInteger lineNumber;
+@property (copy) NSString *lineText;
+
+@end
