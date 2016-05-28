6a49041d494e111b7bdd663052617bcc795269fe
Amanda Wixted <amanda@amandawixted.com>
2016-05-28 15:28:28 -0500

    title the bookmarks window

--- AWBookmarks/AWBookmarksWindowController.h
+++ AWBookmarks/AWBookmarksWindowController.h
@@ -10,4 +10,6 @@
 
 @interface AWBookmarksWindowController : NSWindowController
 
+
+
 @end
--- AWBookmarks/AWBookmarksWindowController.m
+++ AWBookmarks/AWBookmarksWindowController.m
@@ -17,7 +17,8 @@
 - (void)windowDidLoad {
     [super windowDidLoad];
     
-    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    self.window.title = @"Bookmarks";
 }
+
 
 @end
