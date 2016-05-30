473b84c927c26ebce1f58c1b85942947c07cc227
Amanda Wixted <amanda@amandawixted.com>
2016-05-29 21:16:07 -0500

    seed with some fake data

--- AWBookmarks/AWBookmarkCollection.m
+++ AWBookmarks/AWBookmarkCollection.m
@@ -25,6 +25,36 @@
     if(self = [super init])
     {
         self.bookmarks = [[NSMutableArray alloc] init];
+        
+        // Just for testing, have some items in the collection
+        /*
+         <__NSArrayM 0x60800105acd0>(
+         <AWBookmarkEntry: 0x608006e2bfe0>
+         filePath: file:///Users/amanda/Playpen/AWBookmarks/AWBookmarks/AWBookmarksWindowController.h
+         lineNumber: 12
+         lineText:@interface AWBookmarksWindowController : NSWindowController
+         ,
+         <AWBookmarkEntry: 0x60800662e560>
+         filePath: file:///Users/amanda/Playpen/AWBookmarks/AWBookmarks/AWBookmarksWindowController.h
+         lineNumber: 14
+         lineText:@property (strong) AWBookmarkCollection *bookmarkCollection;
+         
+         )
+         */
+        
+        AWBookmarkEntry *one = [[AWBookmarkEntry alloc] init];
+        one.filePath = [NSURL URLWithString:@"file:///Users/amanda/Playpen/AWBookmarks/AWBookmarks/AWBookmarksWindowController.h"];
+        one.lineNumber = @(12);
+        one.lineText = @"@@interface AWBookmarksWindowController : NSWindowController";
+        [self.bookmarks addObject:one];
+        
+        AWBookmarkEntry *two = [[AWBookmarkEntry alloc] init];
+        two.filePath = [NSURL URLWithString:@"file:///Users/amanda/Playpen/AWBookmarks/AWBookmarks/AWBookmarksWindowController.h"];
+        two.lineNumber = @(14);
+        two.lineText = @"@@property (strong) AWBookmarkCollection *bookmarkCollection;";
+        [self.bookmarks addObject:two];
+        
+        
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(performBookmarkThisLine:) name:@"AW_contextMenuBookmarkOptionSelected" object:nil];
         
     }
